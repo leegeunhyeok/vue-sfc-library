2137c96003785d8f13e0f1390eefb57ff4312477
leegeunhyeok <lghlove0509@naver.com>
2019-06-18 22:29:30 +0900

    ADD: dev script, App.vue code

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <Vest/>
+  </div>
+</template>
+
+<script>
+import Vest from './components/Vest'
+
+export default {
+  name: 'app',
+  components: {
+    Vest
+  }
+}
+</script>
+
+<style>
+.app {
+  background-color: #eee;
+}
+</style>
