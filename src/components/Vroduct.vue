1112674257483e62940f7d240f8dd16bbcbd35b0
leegeunhyeok <lghlove0509@naver.com>
2019-06-19 12:26:24 +0900

    UPDATE: Vroduct.vue (Sample feature)

--- src/components/Vroduct.vue
+++ src/components/Vroduct.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="vroduct">
+  <div class="vroduct" :style="{ backgroundColor: currentColor }">
     {{ message }}
   </div>
 </template>
@@ -9,16 +9,45 @@ export default {
   name: 'vroduct',
   data () {
     return {
-      message: 'Vue pRODUCT'
+      message: 'Vue pRODUCT',
+      loop: null,
+      colorList: [
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'navy',
+        'purple'
+      ],
+      currentColor: 'red'
+    }
+  },
+  created () {
+    this.loop = setInterval(this.changeColor, 500)
+  },
+  methods: {
+    changeColor () {
+      let idx = this.colorList.indexOf(this.currentColor)
+      if (idx + 1 === this.colorList.length) {
+        idx = -1
+      }
+      this.currentColor = this.colorList[++idx]
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+$duration: .3s;
+
 .vroduct {
   font-size: 2rem;
   font-weight: bold;
-  color: crimson;
+  -webkit-transition: $duration;
+     -moz-transition: $duration;
+      -ms-transition: $duration;
+       -o-transition: $duration;
+          transition: $duration;
 }
 </style>
