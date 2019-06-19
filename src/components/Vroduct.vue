82e3996550363304067bd42eb2bbf4f6d18d744f
leegeunhyeok <lghlove0509@naver.com>
2019-06-19 10:54:20 +0900

    ADD: Vroduct.vue

---
+++ src/components/Vroduct.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="vroduct">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vroduct',
+  data () {
+    return {
+      message: 'Vue pRODUCT'
+    }
+  }
+}
+</script>
+
+<style>
+.vroduct {
+  font-size: 2rem;
+  font-weight: bold;
+  color: crimson;
+}
+</style>
